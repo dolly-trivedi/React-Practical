04ed0e20c3cb92a1c288f19dfcf3019457b1b5fa
dolly-techtic <dolly@techtic.agency>
2023-04-07 19:28:56 +0530

    react practical one

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+<div className="App">
+<form>
+      <label>
+        Name:
+        <input type="text"  />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+    </div>
+ );
+}
+
+export default App;
